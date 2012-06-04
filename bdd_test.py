06db9282af78ed7ff431f145b12e8d574381b2be
Kai <kus@thinkpad.(none)>
2012-06-04 15:28:06 +0200

    slighty changes in the test file

--- bdd_test.py
+++ bdd_test.py
@@ -2,9 +2,13 @@ import bdd
 
 #bdd.Node(variable, TrueNode, FalseNode)
 
-n1=bdd.Node('x1', bdd.Node.T, bdd.Node.F)
+n1=bdd.Node('x1', bdd.Node.T, bdd.Node.T)
 n2=bdd.Node('x1', bdd.Node.F, bdd.Node.F)
 
+n2.setTrueNode(bdd.Node.T)
+n2.setFalseNode(bdd.Node.T)
+
+print n2
 n3=bdd.Node('x2', n1, n2)
 
 n4=bdd.Node('x3', n3, n3)
@@ -16,4 +20,9 @@ n5= bdd.Node('x4', n4, n4)
 #print n4
 
 
-print n4.dotPrint2()
+#print n2.dotPrint2()
+
+
+gen=bdd.createTree(4)
+
+gen.dotPrint2()
