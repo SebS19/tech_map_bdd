14f3d181357f808731f5bf05cd12c04b1e7865c0
Kai <kus@thinkpad.(none)>
2012-06-18 21:31:11 +0200

    only main2.py :)

--- main2.py
+++ main2.py
@@ -1,9 +1,10 @@
 import boolfunction as bf
 import copy
+import bdd
 
 #------- file read ---------------------------------------------
 
-f = open('absp.pla','r')
+f = open('absp2.pla','r')
 #f = open('blif_src/spla.pla','r')
 #f = open('blif_src/apex2.pla','r')
 #f = open('blif_src/seq.pla','r')
@@ -15,8 +16,9 @@ f = open('absp.pla','r')
 
 
 content = f.readlines()
-
 f.close()
+
+
 
 #------- splitting header / boundset / freeset  ---------------------
 
@@ -38,26 +40,61 @@ for line in content2:
 	if line[0][0] != '.' and len(line) == 2:
 		equations.append([ line[0] , line[1][:-1]])
 
+
+#------- create minterms from blif equations  ---------------------
+
 print "BLIF input On set:"
-maxterm = bf.Maxterm(0)		# 0 is index, increment for every output 
+
+maxtermArray = []
+for i in range(outputs):
+	maxtermArray.append(bf.Maxterm(i))					# 0 is index, increment for every output 
+
 for line in equations:
 
-	for x in range(outputs):
+	for output in range(1):			# for every output
 
-		if line[1][x] == '1':
+		if line[1][output] == '1':			# select the line with an 1 at the end to create the minterm
 			print line[0]
-			maxterm.addMinterm(bf.buildMinterm(line[0]))
+			maxtermArray[output].addMinterm(bf.buildMinterm(line[0]))
 
+
+
+
+print maxtermArray[0]
+
+resultTree = bdd.doShannon(maxtermArray[0],1, inputs)
+
+print type(resultTree)
+#print resultTree
+resultTree.dotPrint2()
+
+
+'''
 # Example for shannon expansion
 print "Maxterm: "
-print maxterm
+print maxtermArray[0]
 print "First expansion with x1: "
-maxterm1 = bf.Maxterm(1)
-maxterm2 = bf.Maxterm(2)
-maxterm1 = copy.deepcopy(maxterm)
-maxterm2 = copy.deepcopy(maxterm)
+
+# allocation
+maxterm1 = copy.deepcopy(maxtermArray[0])
+maxterm2 = copy.deepcopy(maxtermArray[0])
+
 maxterm1.setLiteralTrue('x1')
 maxterm2.setLiteralFalse('x1')
 print "x1=1:", maxterm1
 print "x1=0:", maxterm2
 
+print maxterm2 == maxterm1
+
+maxterm1.setLiteralTrue('x2')
+print "x1=1:", maxterm1
+maxterm1.setLiteralTrue('x3')
+print "x1=1:", maxterm1
+maxterm1.setLiteralTrue('x4')
+print "x1=1:", maxterm1
+
+print maxterm1 ==  1
+print maxterm1 == [1]
+print maxterm1 == 1
+
+'''
