2ca850ec007c499eaa5dd2c0ed9b42f19f17c932
Kai <kus@thinkpad.(none)>
2012-06-18 19:15:58 +0200

    C D big step to shannons expansions

--- bdd.py
+++ bdd.py
@@ -60,6 +60,10 @@ class Node(object):
 		else:
 			return False
 
+
+
+
+
 	def makeQRBDD(self):
 
 		compareSet=[self]
@@ -110,9 +114,6 @@ class Node(object):
 					compareSet.append(knoten.trueNode)
 				
 
-
-
-
 	def dotPrint(self):
 		
 		nodeList= [self]
@@ -158,10 +159,9 @@ class Node(object):
 
 
 
-
 def adjust(stringInput, rootNode):
-	currentNode = rootNode
-	currentDepth = 0
+	currentNode 	= rootNode
+	currentDepth 	= 0
 	# getting the last variable node
 	for literal in stringInput[:-1]:
 		currentDepth += 1	
@@ -179,8 +179,6 @@ def adjust(stringInput, rootNode):
 		currentNode.setTrueNode(Node.T)
 	if stringInput[-1] == '0' or stringInput[-1] == '-':	
 		currentNode.setFalseNode(Node.T)
-
-
 
 def createTree(n):
 	
@@ -221,10 +219,79 @@ def getAllOnPaths(treeheight, rootNode, way):
 	else:
 		return getAllOnPaths(treeheight-1, rootNode.trueNode, way+'1'), getAllOnPaths(treeheight-1, rootNode.falseNode, way+'0')
 
-
 # auxiliary function to get height of a tree
 def getHeight(rootNode):
 	if type(rootNode.trueNode) != Node:
 		return 1
 	else:
 		return getHeight(rootNode.trueNode) + 1
+
+
+def doShannon(maxterm, level, height):
+
+
+	maxTermT = copy.deepcopy(maxterm)
+	maxTermF = copy.deepcopy(maxterm)
+
+	# shannon expansion
+	maxTermT.setLiteralTrue( 'x' + str(level))
+	maxTermF.setLiteralFalse('x' + str(level))
+
+	print "--- aktueller maxTerm ---- \n"
+	print maxterm
+
+	print "\n maxtermT \n"
+	print maxTermT
+
+
+	print "\n maxtermF \n"
+	print maxTermF
+
+	print "\n ----------------------"
+
+	if level==height:
+		
+		if maxTermT == [0] and maxTermF == [0]:
+			return Node('x'+str(level), Node.F, Node.F)
+		elif maxTermT == [0] and maxTermF == [1]:
+			return Node('x'+str(level), Node.F, Node.T)
+		elif maxTermT == [1] and maxTermF == [0]:
+			return Node('x'+str(level), Node.T, Node.F)
+		elif maxTermT == [1] and maxTermF == [1]:
+			return Node('x'+str(level), Node.T, Node.T)
+		else:
+			raise SyntaxError("irgendwas stimmt nicht")
+	
+	else:
+		
+		if maxTermF == maxTermT:
+			temp = doShannon(maxTermF,level+1, height)
+			return Node('x' + str(level), temp, temp)
+		else:
+			return Node('x' + str(level), doShannon(maxTermT,level+1,height), doShannon(maxTermF, level+1,height) )
+
+
+
+
+'''
+	if level == 0:
+		if maxterm == [1]:
+			return Node.T
+		if maxterm == [0]:
+			return Node.F
+		elif:
+			raise SyntaxError("wrong level specified, looser")
+
+	if maxterm == [1] or maxterm == [0]:
+		self.__trueNode  = makeChildNode(maxterm, level-1)
+		self.__falseNode = self.__trueNode
+
+	else:
+
+
+
+		self.__trueNode  = Node(	'x' + str(level),\
+									makeChildNode( maxTermT, level-1),\
+									makeChildNode( maxTermF, level-1) )
+		self.__falseNode =  
+'''
